ed0314af5829de5de4fa69a52a97992b0e968343
Manolo Boucard Bocciarelli <manoloboucboc@gmail.com>
2026-09-22 15:05:02 +0200

    comment well the main and config files

--- src/config.h
+++ src/config.h
@@ -1,19 +1,23 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/*Basic ESP32 SPI configuration
+/*Basic ESP32 SPI configuration used for the diplay and SD module (see SPI.h configuration)
 SCL(SCK)=18
 SDA(MOSI)=23
 */
+
+//Pins 2, 5, 12, 15  use for boot strap but works with this application
+//Pins 34, 35 are input only
+
 //Display pins
-#define DISPLAY_CS 5
-#define DISPLAY_BUSY 22
-#define DISPLAY_RES 19
-#define DISPLAY_DC 21
+#define DISPLAY_CS          5  
+#define DISPLAY_BUSY        22
+#define DISPLAY_RES         19
+#define DISPLAY_DC          21
 
 //Light pin
-#define LIGHT_PIN   17
-#define BACKLIGHT_PIN  13
+#define LIGHT_PIN           17
+#define BACKLIGHT_PIN       13
 
 //Button pins
 #define SWITCH_LIGHT_PIN    16
@@ -23,7 +27,17 @@ SDA(MOSI)=23
 #define BUTTON_PARAM_PIN    33
 
 //RTC pins
-#define RTC_SDA 27  
-#define RTC_SCL 14
+#define RTC_SDA             27  
+#define RTC_SCL             14
+
+// Audio amplifier (MAX98357A)
+#define AMP_EN              25
+#define LRCLK               26
+#define BCLK                12  
+#define DIN                 34
+
+//SD pins, SCK and MOSI are shared with the display
+#define SD_CS               2  
+#define SD_MISO             35
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,84 @@
+//Global libraries
 #include <Arduino.h>
 #include "config.h"
+
+//Hardware functions
 #include "hardware/lighting.h"
 #include "hardware/button.h"
-#include "modes/light_mode.h"
 #include "hardware/screen.h"
 #include "hardware/rtc.h"
+
+//Modes functions
+#include "modes/light_mode.h"
 #include "modes/menu_mode.h"
 #include "modes/clock.h"
 
+//Variables
 static int lastDisplayedHour = -1;
 static int lastDisplayedMinute = -1;
+static int lastDisplayedDay = -1;
+static int lastDisplayedMonth = -1;
+static int lastDisplayedYear = -1;
+
 static bool wasMenuActive = false;
 
+//Function for configuring all hardware and modes run once at first power on
 void setup()
 {
+    //Begin serial communication for debugging
     Serial.begin(115200);
 
+    //Hardware initialisation
     rtc_init();
     buttons_init();
-    light_mode_init();
     screen_init();
-    screen_show_home_complete(rtc_get_hour(), rtc_get_minute(), false, false, true, false, false);
+
+    //Modes initialisation
+    light_mode_init();
     menu_init();
+
+    //Initial display
+    screen_show_home_complete(rtc_get_hour(), rtc_get_minute(), false, false, true, false, false);
 
     Serial.println("================================");
     Serial.println("        Sunrise Simulator       ");
     Serial.println("================================");
 }
 
+//Main function that runs continuously
 void loop()
-{
+{   
+    //Reading the actions on the buttons
     buttons_update();
     backlight_update();
+    
+    //We only adjust the colour and brightness of the light when the menu is not active (the ‘+’ and ‘-’ buttons are used for other functions within the menu)
     if (!menu_is_active())
     {
         light_mode_update();
     }
+    
+    //We increment the software clock and update the internal variables
     rtc_update();
     clock_update();
     menu_update();
 
+    //We collect the information before it is displayed
     int hour = rtc_get_hour();
     int minute = rtc_get_minute();
+    uint8_t day = rtc_get_day();
+    uint8_t month = rtc_get_month();
+    uint16_t year = rtc_get_year();
 
+    //Depending on the values of the internal variables, we adjust the display
+    //If the user exits the menu, we redraw everything
     if (wasMenuActive && !menu_is_active()){
         screen_show_home_complete(hour, minute, false, false, true, false, false);
         lastDisplayedHour = hour;
         lastDisplayedMinute = minute;
     }
 
+    //If the time has changed, we only update the time
     if (!menu_is_active())
     {
         if (hour != lastDisplayedHour || minute != lastDisplayedMinute)
@@ -58,5 +88,15 @@ void loop()
             screen_show_home(hour, minute);
         }
     }
+
+    //If the date has changed, we only update the date
+    if (day != lastDisplayedDay || month != lastDisplayedMonth || year != lastDisplayedYear)
+    {
+        screen_update_home_date();
+
+        lastDisplayedDay = day;
+        lastDisplayedMonth = month;
+        lastDisplayedYear = year;
+}
     wasMenuActive=menu_is_active();
 }
